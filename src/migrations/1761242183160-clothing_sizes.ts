d213357f1f65c3e0bce7619e83b613046f38ddb7
PVeeeeeee <pvitprss2004@gmail.com>
2025-10-23 15:03:00 -0300

    feat: migration da tabela de tamanhos das roupas

---
+++ src/migrations/1761242183160-clothing_sizes.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateClothingSizesTable1680000000000 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'clothing_sizes',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'size',
+                        type: 'varchar',
+                        length: '255',
+                        isUnique: true,
+                    },
+                ],
+            }),
+        );
+
+        await queryRunner.query(`
+            INSERT INTO clothing_sizes (size) VALUES
+            ('PP'), ('P'), ('M'), ('G'), ('GG')
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('clothing_sizes');
+    }
+}
