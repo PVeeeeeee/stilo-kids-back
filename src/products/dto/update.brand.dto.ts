aa36f1d0f8647f6e9ea10566364940471d433bc1
PVeeeeeee <pvitprss2004@gmail.com>
2025-10-23 14:16:00 -0300

    Atualização de Branch

--- src/products/dto/update.brand.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
-import CreateBrandDto from './create.brand.dto';
-
-export default class UpdateBrandDto extends PartialType(CreateBrandDto) {}
